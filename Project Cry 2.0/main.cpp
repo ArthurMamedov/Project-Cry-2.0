957db24c455308bba4539f4e4babdad8665dafc5
Артур Мамедов <arturmamedov1212@gmail.com>
2021-01-29 02:38:08 +0200

    Final

--- Project Cry 2.0/main.cpp
+++ Project Cry 2.0/main.cpp
@@ -11,26 +11,8 @@
 
 int main(int argc, char** argv) {
 	//cry [encrypt/decrypt/help/enc/dec] [filepath1 filepath2 ...] [aes/gost/blowfish] [ecb/cbc/cfb/ofb/ctr] [key]
-	//char p[] = "Hello there!";
-	//std::cout << (int*)p << std::endl;
-	//auto lmbd = [&p]() {
-	//	p[0] = 'T';
-	//	std::cout << (int*)p << std::endl;
-	//};
-	//lmbd();
-	//std::cout << p << std::endl;
 	
-	std::unique_ptr<ICore> core(new AesCore("1234567890abcdef"));
-	std::unique_ptr<ICryptor> cryptor(new CtrCryptor(std::move(core)));
-	//auto p = dynamic_cast<CtrCryptor*>(cryptor.get());
-	//p->set_init_vec((uint8_t*)"abcdef0123456789");
-	FileCryptor file(std::move(cryptor));
-	file.encrypt_file("D:\\Projects\\C++\\ProjectCry 2.0\\Project Cry 2.0\\Project Cry 2.0\\tuxedo.txt");
-	file.decrypt_file("D:\\Projects\\C++\\ProjectCry 2.0\\Project Cry 2.0\\Project Cry 2.0\\tuxedo.txt.enc");
-	std::cout << "FINISH!" << std::endl;
-
-	
-	/*if (argc < 6 && argv[1] != "help") {
+	if (argc < 6 && argv[1] != "help") {
 		std::cout << "Not enought arguments." << std::endl;
 		return 0;
 	} else if (argc < 6 && argv[1] == "help") {
@@ -56,7 +38,7 @@ int main(int argc, char** argv) {
 		}
 	} catch (const std::exception& ex) {
 		std::cout << ex.what() << std::endl;
-	}*/
+	}
 
 	return 0;
 }
